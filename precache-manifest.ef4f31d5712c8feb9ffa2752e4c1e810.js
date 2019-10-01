241baf80568418d60eebc062c0400286de488f3c
Daliri.ha <daliri.ha@gmail.com>
2019-10-01 22:53:36 +0330

    Updates

---
+++ precache-manifest.ef4f31d5712c8feb9ffa2752e4c1e810.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f4e42a7d931f7c9ddd39704f26c159",
+    "url": "/reactjs-resume/index.html"
+  },
+  {
+    "revision": "546b5bbf664c21056fa4",
+    "url": "/reactjs-resume/static/css/main.7eccb62c.chunk.css"
+  },
+  {
+    "revision": "90af7d4a1b7bfe4065cb",
+    "url": "/reactjs-resume/static/js/2.19b1660c.chunk.js"
+  },
+  {
+    "revision": "546b5bbf664c21056fa4",
+    "url": "/reactjs-resume/static/js/main.ac14d937.chunk.js"
+  },
+  {
+    "revision": "212e8732a743f9721c23",
+    "url": "/reactjs-resume/static/js/runtime~main.82d8b1b2.js"
+  }
+]);
